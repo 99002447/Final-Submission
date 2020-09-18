062c73ef0f706125c2728d12011ce9258a1281e8
99002447 <71481211+99002447@users.noreply.github.com>
2020-09-19 01:55:06 +0530

    Update main.c

--- Calculator/main.c
+++ Calculator/main.c
@@ -13,7 +13,7 @@ int calculator_operand1 = 0;
 int calculator_operand2 = 0;
 
 /* Valid operations */
-enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE, POWER, ROOT, FACTORIAL, EXIT };
+enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE, POWER, ROOT, FACTORIAL, LOG, LOG10, EXIT };
 
 /* Display the menu of operations supported */
 void calculator_menu(void);
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 void calculator_menu(void)
 {
     printf("\nAvailable Operations\n");
-    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Power\n6. Root\n7. Factorial \n8.Exit");
+    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Power\n6. Root\n7. Factorial \n8. Log \n9. Log10 \n10.Exit");
     printf("\n\tEnter your choice\n");
    
      __fpurge(stdin);
@@ -124,7 +124,23 @@ void calculator_menu(void)
             __fpurge(stdin);
             getchar();
             break;  
-        case 8:
+          case LOG:
+            printf("\n\tLog of %d is %d\nEnter to continue", 
+            calculator_operand1,
+            logarithm(calculator_operand1));
+            
+            __fpurge(stdin);
+            getchar();
+            break; 
+          case LOG10:
+            printf("\n\tLog of %d is %d\nEnter to continue", 
+            calculator_operand1,
+            logarithm10(calculator_operand1));
+            
+            __fpurge(stdin);
+            getchar();
+            break; 
+        case 10:
             exit(0);
             break;
         default:
