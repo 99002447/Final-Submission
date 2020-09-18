db1336bb048bd0ad77007fe14e36c1bd10d2d039
99002447 <71481211+99002447@users.noreply.github.com>
2020-09-18 17:19:42 +0530

    Update calculator_operations.c

--- Calculator/src/calculator_operations.c
+++ Calculator/src/calculator_operations.c
@@ -33,3 +33,13 @@ int power(int operand1, int operand2)
     else
         return pow(operand1,operand2);
 }
+
+int root(int operand1, int operand2)
+{
+    if(0 == operand2)
+        return 0;
+    if(1 == operand2)
+        return operand1;
+    else
+        return pow(operand1,(1/operand2));
+}
