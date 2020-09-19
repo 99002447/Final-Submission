40bf985dac988ba324d626c227243cdc480925e2
stepin104251 <69070923+stepin104251@users.noreply.github.com>
2020-09-19 13:01:06 +0530

    Update calculator_operations.c

--- 3_Implementation/src/calculator_operations.c
+++ 3_Implementation/src/calculator_operations.c
@@ -85,10 +85,25 @@ int dollartoinr(int operand1)
   return operand1*73;
 }
 
-int prime(int operand1)
+
+  int prime(int operand1)
 {
-  if (operand1 == 2 || (operand1%2==0))
-    return 1;
- else 
-	return 0;
+    /* return -1 if a negative number*/
+    int i,count=0;
+    if (operand1<0)
+        return -1;
+
+    for(i=1;i<=operand1;i++)
+    {
+        if((operand1%i)==0)
+        {
+            count++;
+        }
+    }
+    /*return 1 for a prime number*/
+    if(count==2)
+        return 1;
+    /*return 0 for a non prime number*/
+    else
+        return 0;
 }
